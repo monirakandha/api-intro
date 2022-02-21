2a11e3d0ab859bad047c9ff95917d535c657dd07
monirhosenakanda <monirhosenakanda@gmail.com>
2022-02-21 23:20:12 +0600

    stringify convet form normal object also normal object strihifield

---
+++ js/app.js
@@ -0,0 +1,31 @@
+//javascript object notation
+//json
+// const user = { id:11, name: 'gorib amair' , job:'actor'}
+// console.log(user);
+
+// const stringified = JSON.stringify(user);
+// console.log(stringified);
+
+
+//complex object
+
+const shop = {
+    name : 'Alia Store',
+    address: 'Ranbir Road',
+    products : ['laptop' , 'Phone', 'pepsi'],
+    profit : 15000,
+    owner : {
+        name : 'Alia Bhat',
+        professon : 'Actor'
+    },
+    isexpensive : false
+
+}
+const shopStringified = JSON.stringify(shop);
+// console.log(shop);
+console.log(shopStringified);
+
+
+//convert stringify to normal
+const converted = JSON.parse(shopStringified);
+console.log(converted);
